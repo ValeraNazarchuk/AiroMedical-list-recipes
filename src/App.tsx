87822d4a219a5b0f838c30bbce17c09dc2765d10
unknown <valera55555nazarchuk@gmail.com>
2023-07-16 15:05:40 +0300

    starting file structure

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './assets/style/main.css'
+// import { CRouter } from './modules/root'
+import { ConfigProvider } from 'antd'
+
+function App() {
+  return (
+    <ConfigProvider>
+      {/* <CRouter /> */}
+    </ConfigProvider>
+  )
+}
+
+export default App
